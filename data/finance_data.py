016709ba5c4a770e6c73a4d6d22c57028149f72b
Holly Everett <1709869@brunel.ac.uk>
2023-06-23 20:32:50 +0000

    code base structure

---
+++ data/finance_data.py
@@ -0,0 +1,4 @@
+#finance_data data structure class
+class finance_data:
+    def __init__(self):
+        pass
---
+++ data/image_data.py
@@ -0,0 +1,4 @@
+#image_data data structure class
+class image_data:
+    def __init__(self):
+        pass
