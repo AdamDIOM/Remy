216e7bdad77956a713ba4f61c85346711a04e850
Holly Everett <1709869@brunel.ac.uk>
2023-06-24 13:08:29 +0000

    added number endpoint

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,16 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from service.scrape_data import get_number
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello from Flask! 2'
+
+
+@app.route('/number')
+def number():
+    return get_number('EUR/USD')
 
